d437e4b96e3d74e49619045bdb5e09c5e9341ec7
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2022-11-29 18:57:12 -0800

    Begin Pid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,10 +5,14 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.SparkMaxRelativeEncoder;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,11 +24,16 @@ import edu.wpi.first.wpilibj.XboxController;
  * project.
  */
 public class Robot extends TimedRobot {
+  private static final double GEARING = 64;
+  private static final double WRIST_POSITION_UP = 0 * GEARING; 
+  private static final double WRIST_POSITION_DOWN = 0 * GEARING;
   CANSparkMax intakeRollers = new CANSparkMax(15, MotorType.kBrushless);
   CANSparkMax wrist = new CANSparkMax(16, MotorType.kBrushless);
   CANSparkMax shooter = new CANSparkMax(17, MotorType.kBrushless);
   CANSparkMax queuer = new CANSparkMax(18, MotorType.kBrushless);
   XboxController controller = new XboxController(0);
+  SparkMaxPIDController wristPid; 
+  SparkMaxRelativeEncoder wristEncoder;
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -33,75 +42,32 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-
-  }
-
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items
-   * like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different
-   * autonomous modes using the dashboard. The sendable chooser code works with
-   * the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-   * chooser code and
-   * uncomment the getString line to get the auto name from the text box below the
-   * Gyro
-   *
-   * <p>
-   * You can add additional auto modes by adding additional comparisons to the
-   * switch structure
-   * below with additional strings. If using the SendableChooser make sure to add
-   * them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {
+    wristPid = wrist.getPIDController();
+    wristPid.setP(0);
+    wristPid.setI(0);
+    wristPid.setD(0);
+    wristPid.setIZone(0);
+    wristPid.setFF(0);
+    wristPid.setOutputRange(-0.5, 0.5);
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
     boolean rolling = controller.getLeftTriggerAxis() > 0.5;
+    boolean backwardsRolling = controller.getLeftBumper();
     if (rolling) {
       intakeRollers.set(0.4);
-    } else {
-      intakeRollers.set(0);
-    }
-
-    boolean backwardsRolling = controller.getLeftBumper();
-    if (backwardsRolling) {
+      wristPid.setReference(WRIST_POSITION_UP, ControlType.kPosition);
+    } else if (backwardsRolling) {
       intakeRollers.set(-0.4);
       queuer.set(-0.5);
+      wristPid.setReference(WRIST_POSITION_UP, ControlType.kPosition);
     } else {
       intakeRollers.set(0);
       queuer.set(0);
+      wristPid.setReference(WRIST_POSITION_DOWN, ControlType.kPosition);
     }
-
-    double wristing = controller.getLeftY();
-    wrist.set(-wristing/2);
 
     boolean shooting = controller.getRightTriggerAxis() > 0.5;
     if (shooting) {
@@ -113,33 +79,9 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {
+  public void robotPeriodic() {
+       SmartDashboard.putNumber("wrist/angle", wristEncoder.getPosition() / GEARING);
   }
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {
-  }
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {
-  }
 }
