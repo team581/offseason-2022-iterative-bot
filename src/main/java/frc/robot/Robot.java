e48bc43770ffb980fd6861b798155a0dff86b42b
Chris <117956866+lechris07@users.noreply.github.com>
2022-12-01 18:26:13 -0800

    starting PID (work in progress) - owen

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -23,6 +24,7 @@ public class Robot extends TimedRobot {
 
   CANSparkMax wrist = new CANSparkMax(16, MotorType.kBrushless);
   XboxController controller = new XboxController(0);
+  SparkMaxPIDController wristPID;
 
   CANSparkMax intakeRollers = new CANSparkMax(15, MotorType.kBrushless);
 
@@ -38,56 +40,22 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    wristPID = wrist.getPIDController();
+    wristPID.setP(0.1);
+    wristPID.setI(0.1);
+    wristPID.setD(0.1);
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items
-   * like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different
-   * autonomous modes using the dashboard. The sendable chooser code works with
-   * the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
-   * chooser code and
-   * uncomment the getString line to get the auto name from the text box below the
-   * Gyro
-   *
-   * <p>
-   * You can add additional auto modes by adding additional comparisons to the
-   * switch structure
-   * below with additional strings. If using the SendableChooser make sure to add
-   * them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-  }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-  }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {
-  }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+
+
+
     double wristSpeed = -controller.getLeftY();
 
     wrist.set(wristSpeed / 3);
@@ -114,33 +82,5 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {
-  }
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {
-  }
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {
-  }
 }
