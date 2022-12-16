0c308ecdc3b94a4aac5babe43ab9972ae343cfd3
Chris <117956866+lechris07@users.noreply.github.com>
2022-12-15 18:54:45 -0800

    update module angle offsets

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,9 +50,9 @@ public class Robot extends TimedRobot {
   private static final double SHOOTER_VELOCITY_IDLE = 700.0 * 360.0 / 60.0;
 
   private final SwerveModule frontLeft = new SwerveModule(new TalonFX(2), new TalonFX(3), new CANCoder(10), Rotation2d.fromDegrees(104.6), "FrontLeft");
-  private final SwerveModule frontRight = new SwerveModule(new TalonFX(4), new TalonFX(5), new CANCoder(11), Rotation2d.fromDegrees(78.95), "FrontRight");
-  private final SwerveModule backLeft = new SwerveModule(new TalonFX(6), new TalonFX(7), new CANCoder(12), Rotation2d.fromDegrees(-148), "BackLeft");
-  private final SwerveModule backRight = new SwerveModule(new TalonFX(8), new TalonFX(9), new CANCoder(13), Rotation2d.fromDegrees(-62.53), "BackRight");
+  private final SwerveModule frontRight = new SwerveModule(new TalonFX(4), new TalonFX(5), new CANCoder(11), Rotation2d.fromDegrees(75.5), "FrontRight");
+  private final SwerveModule backLeft = new SwerveModule(new TalonFX(6), new TalonFX(7), new CANCoder(12), Rotation2d.fromDegrees(31), "BackLeft");
+  private final SwerveModule backRight = new SwerveModule(new TalonFX(8), new TalonFX(9), new CANCoder(13), Rotation2d.fromDegrees(121), "BackRight");
 
 
   private final CANSparkMax wrist = new CANSparkMax(16, MotorType.kBrushless);
